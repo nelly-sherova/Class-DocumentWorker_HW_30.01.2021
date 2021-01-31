a56f8b617d32b3292415117f05b51c65ea30e49f
unknown <nellysherova@mail.ru>
2021-01-31 23:59:19 +0300

    Начало работы

---
+++ DocumentWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DocumentWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
